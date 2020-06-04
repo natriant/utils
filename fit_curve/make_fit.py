7978e49080e90b4559b17df97cd4314518375f44
Natalia Triantafyllou <natalia.triantafyllou@cern.ch>
2020-06-04 16:44:17 +0200

    update fit curve

--- fit_curve/make_fit.py
+++ fit_curve/make_fit.py
@@ -13,21 +13,30 @@ dataYLim = ax.get_ylim()
 best_distribution, best_fit_name, best_fit_params = best_fit_distribution(data, 200, ax)
 best_dist = getattr(st, best_fit_name)
 
+print('The best fit is a {} distribution'.format(best_fit_name))
+
 param_names = (best_dist.shapes + ', loc, scale').split(', ') if best_dist.shapes else ['loc', 'scale']
 param_str = ', '.join(['{}={}'.format(k,v) for k,v in zip(param_names, best_fit_params)])
 
 print('param_names',param_names)
 print('param_Strings', param_str)
-print('best fit parameters', best_fit_params)
-#print('stats', best_distribution.stats(best_fit_params[0], loc=best_fit_params[1], scale=best_fit_params[2], moments='mvsk'))
-print('stats', best_distribution.stats(loc=best_fit_params[0], scale=best_fit_params[1], moments='mvsk'))
 
+
+# Compute the mean and the variance of the distribution
+if best_fit_name == 'expon':
+    mean, var = best_distribution.stats(loc=best_fit_params[0], scale=best_fit_params[1], moments='mv')
+elif best_fit_name == 'gamma':
+    mean, var = best_distribution.stats(best_fit_params[0], loc=best_fit_params[1], scale=best_fit_params[2], moments='mv')
+elif best_fit_name =='beta':
+     mean, var = best_distribution.stats(best_fit_params[0], best_fit_params[1], loc=best_fit_params[2], scale=best_fit_params[3], moments='mv')
+else:
+    print('computing the mean and variance of this distibution is not yet implemented')
+
+print('mu={}, var={}, simga={}'.format(mean, var, np.sqrt(var)))
 
 # Update plots
+# Update plots
 ax.set_ylim(dataYLim)
-ax.set_title(u'All Fitted Distributions')
-ax.set_xlabel('Betatron tune')
-ax.set_ylabel(r'$\rho (\nu_b)$')
 
 # Make PDF with best params
 pdf = make_pdf(best_dist, best_fit_params)
@@ -38,10 +47,13 @@ ax = pdf.plot(lw=2, label='PDF', legend=True)
 data.plot(kind='hist', bins=50, normed=True, alpha=0.5, label='Data', legend=True, ax=ax)
 
 param_names = (best_dist.shapes + ', loc, scale').split(', ') if best_dist.shapes else ['loc', 'scale']
-param_str = ', '.join(['{}={}'.format(k,v) for k,v in zip(param_names, best_fit_params)])
+param_str = ', '.join(['{}={:.3f}'.format(k,v) for k,v in zip(param_names, best_fit_params)])
 dist_str = '{}({})'.format(best_fit_name, param_str)
 
-ax.set_title(u'Bbest fit distribution \n' + dist_str)
+ax.set_title('Best fit distribution \n {} \n mean={:.3f}, sigma={:.5f}'.format(dist_str, mean, np.sqrt(var) ))
+
+
 ax.set_xlabel('Betatron tune')
 ax.set_ylabel(r'$\rho (\nu_b)$')
+plt.savefig('study_name.png')
 plt.show()
