e73c4e16106207ee75be6ed5dfe3e92a7db4b44d
natriant <nataliatriantafyllou@gmail.com>
2020-06-08 16:51:42 +0200

    anlaysis in chunks

--- noise_studies_playground/colored_noise/peaked_noise_spectra.py
+++ noise_studies_playground/colored_noise/peaked_noise_spectra.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # noise parameters
 phi_0 = 1e-8  # amplitude of noise
-Delta_psi = 0.32  # the peak of the spectrum
+Delta_psi = 0.18  # the peak of the spectrum
 
 psi_t_list = []
 psi_t = 0
@@ -12,7 +12,7 @@ turns = np.arange(1000)  # np.arange() gives sampling 1 which corresponds to tur
 
 # parameters for ksi
 mean = 0.0
-std = 14
+std = 0.08
 for i in turns:
     psi_t_list.append(psi_t)
     ksi = np.random.normal(mean, std)  # different seed on each turn
@@ -21,7 +21,7 @@ for i in turns:
 # Construct the noise signal
 phi_noise = phi_0*np.cos(psi_t_list)
 
-plt.ylabel("Amplitude")
+plt.ylabel("Noise amplitude (arbitrary units)")
 plt.xlabel("Time [s]")
 plt.plot(turns, phi_noise, '.-')
 plt.show()
@@ -39,7 +39,8 @@ N = phi_noise.size
 
 f = np.linspace(0, 1 / T, N)  # 1/T = frequency
 
-plt.ylabel("Amplitude")
+plt.ylabel("Noise amplitude (arbitrary units)")
 plt.xlabel("Frequency (tune units)")
-plt.plot(f[:N // 2], np.abs(fft)[:N // 2] * 1 / N) # 1 / N is a normalization factor
+plt.plot(f[:N // 2], np.abs(fft)[:N // 2] * 1 / N)  # 1 / N is a normalization factor
 plt.show()
+plt.close()
