758b442e5b39467208d03148cdce706e7e5c0510
natriant <nataliatriantafyllou@gmail.com>
2020-12-08 00:09:26 +0100

    cmpt rms tune spread taking into account the coupling ceofficient

--- cmpt_TuneSpreads.py
+++ cmpt_TuneSpreads.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def chromatic_tune_spread(dpp_rms, order, Qp):
     if order == 1:
         print('1st chromaticity')
@@ -9,9 +11,21 @@ def chromatic_tune_spread(dpp_rms, order, Qp):
 
 
 def amplitude_detuning_x(Jx, Jy, a_xx, a_xy):
+        # compatible with pyheadtail
         return a_xx * 2 * Jx + a_xy * 2 * Jy
 
 
 def amplitude_detuning_y(Jx, Jy, a_yy, a_xy):
     return a_yy * 2 * Jy + a_xy * 2 * Jx
+
+
+def rms_amplitude_detuning_x(Jx, Jy, a_xx, a_xy):
+        # compatible with pyheadtail
+        # Jx, Jy: arrays
+        return 2*np.sqrt((a_xx*np.std(Jx))*2+(a_xy*np.std(Jy))*2)
+
+
+def rms_amplitude_detuning_y(Jx, Jy, a_yy, a_xy):
+    # Jx, Jy: arrays
+    return 2*np.sqrt((a_yy*np.std(Jy))*2+(a_xy*np.std(Jx))*2)
 
